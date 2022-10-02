18e5b083e2baffe0b57ad4593aec5362ca5ffefa
Irfan <irfan.ik227@gmail.com>
2022-10-03 02:18:58 +0500

    NFT contract create done

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,17 @@
 require("@nomicfoundation/hardhat-toolbox");
+const fs = require('fs');
+const privateKey = fs.readFileSync('.secret').toString();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+    mainnet: {
+      url: 'https://mainnet.infura.io/v3/c71e3763c19946d08763a84cd4624236',
+      accounts: [privateKey],
+    },
+  },
   solidity: "0.8.17",
 };
